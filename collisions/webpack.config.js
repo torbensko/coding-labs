84b4e7c43975f4f49f9c3219e88945ae5aa38c05
Torben Sko <torben.sko@nextpracticehealth.com>
2022-04-06 22:20:57 +1000

    Init setup with webpack, live reload and P5.js

---
+++ collisions/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const extract = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'ts-loader',
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: extract.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new extract({
+      filename: 'bundle.css'
+    })
+  ],
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: false,
+    port: 9000,
+  },
+}
