a2902c5e2b81e8b12da9afc2fc9836744c95d7e6
Torben Sko <torben.sko@nextpracticehealth.com>
2022-04-10 16:15:26 +1000

    Import clean up

--- collisions/src-example/index.tsx
+++ collisions/src-example/index.tsx
@@ -2,36 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import P5 from "p5";
-import { random } from "lodash";
 import { Button } from "@mui/material";
 
 import { width, height } from "./helpers/constants";
 import { Ball } from "./helpers/Ball";
+import { createBall } from "./helpers/createBall";
 
-const balls: Ball[] = [];
-
-const createBall = () => {
-  const size = 100;
-  let collision = true;
-  let ball: Ball = null;
-  do {
-    ball = new Ball(
-      size,
-      random(width - size) + size / 2,
-      random(height - size) + size / 2
-    );
-    collision =
-      balls.map((b) => b.willCollide(ball)).filter((n) => n).length > 0;
-  } while (collision);
-  balls.push(ball);
-};
+export const balls: Ball[] = [];
 
 const sketch = (p5: P5) => {
   p5.setup = () => {
     const canvas = p5.createCanvas(width, height);
     canvas.parent("app");
     for (let i = 0; i < 2; i++) {
-      createBall();
+      balls.push(createBall());
     }
   };
 
